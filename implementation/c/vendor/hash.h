b8fc80b60e7955356dcaf432021351ec39ab152d
Jun Woo Shin <shin.junwoo97@gmail.com>
2017-11-02 17:27:29 -0400

    vendor libraries

---
+++ implementation/c/vendor/hash.h
@@ -0,0 +1,16 @@
+#ifndef ASH_VENDOR_HASH_H
+#define ASH_VENDOR_HASH_H
+
+
+uint64_t hashJDB2(unsigned char*);
+
+uint64_t hashSBDM(unsigned char*);
+
+void hashMurmur3_x86_32(const void*, const int, const uint32_t, void*);
+
+void hashMurmur3_x86_128(const void*, const int, const uint32_t, void*);
+
+void hashMurmur3_x64_128(const void*, const int, const uint32_t, void*);
+
+
+#endif
