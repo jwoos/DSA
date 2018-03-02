b13efe5e9a9abbb4bbc4253838ed7e34765d78e9
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-03-02 08:21:01 +0000

    common

---
+++ implementation/c/common.h
@@ -0,0 +1,18 @@
+#ifndef DSA_COMMON_H
+#define DSA_COMMON_H
+
+
+#define DSA_DATA_DEFAULT_SIZE 16
+#define DSA_DATA_INCREASE_FACTOR 2
+
+
+enum Resize {
+	ADD,
+	MULTIPLY,
+	SUBTRACT,
+	DIVIDE,
+	SET
+};
+
+
+#endif
